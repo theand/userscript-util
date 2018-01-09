a8309f7b960169a2378d7485c92e2a86362693e0
Heesang Chae <theand@gmail.com>
2018-01-09 19:30:09 +0900

    clipboard event using clipboard.js

---
+++ clipboard.js
@@ -0,0 +1,26 @@
+window.userscript_util = window.userscript_util || {};
+
+function bindClipboardAction(sel){
+    if( !sel ){
+        sel = "#copy-button";
+    }
+    const clipboard = new Clipboard(sel);
+
+    clipboard.on("success", function (e) {
+        // console.info("Action:", e.action);
+        // console.info("Text:", e.text);
+        // console.info("Trigger:", e.trigger);
+
+        e.clearSelection();
+        document.querySelector(sel).style.backgroundColor = "#d9534f";
+        document.querySelector(sel).style.borderColor = "#d43f3a";
+
+    });
+
+    clipboard.on("error", function (e) {
+        console.error("Action:", e.action);
+        console.error("Trigger:", e.trigger);
+    });
+
+}
+window.bindClipboardAction = bindClipboardAction;
