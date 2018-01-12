a0b9d7a8540e1694c4a66e0beb5ad6fbc89941ba
Heesang Chae <theand@gmail.com>
2018-01-12 18:08:49 +0900

    extract remove param routine

---
+++ param.js
@@ -0,0 +1,32 @@
+window.userscript_util = window.userscript_util || {};
+
+
+function removeParam(URI, REDUNDANT_PARAM_KEY, GM_info){
+    try {
+        //querystring to object
+        const uri = new URI(window.location);
+        const originalQuery = uri.query();
+
+        //filter object key
+        REDUNDANT_PARAM_KEY
+            .filter(e => uri.hasQuery(e))
+            .map(e => uri.removeQuery(e));
+
+        //objet to querystring
+        const newQuery = uri.query();
+        console.log(newQuery);
+
+        //relocate if filtered
+        if (originalQuery !== newQuery) {
+            console.log(`UserScript : change to filtered location : ${uri.toString()}`);
+            history.pushState(null, null, uri.toString());
+        } else {
+            console.log("UserScript : no changes");
+        }
+
+    } catch (err) {
+        userscript_util.logGmError(GM_info, err);
+    }
+}
+
+window.userscript_util.removeParam = removeParam;
