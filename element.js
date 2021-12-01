4ed0602313521d2d00a7b1e08f27fcda8575a571
Heesang Chae <theand@gmail.com>
2021-12-01 21:17:00 +0900

    attachLinkAddressExtractedFrom 에서 br 대신 ul li 로 생성하는 옵션 지원

--- element.js
+++ element.js
@@ -37,6 +37,7 @@ window.userscript_util.element.CopyDiv = `
         border-color: #4cae4c;
         display: inline-block;
         padding: 6px 12px;
+        margin-bottom: 2em;
         font-size: 14px;
         font-weight: 400;
         line-height: 1.42857143;
@@ -51,8 +52,7 @@ window.userscript_util.element.CopyDiv = `
         border-radius: 4px;
         -webkit-appearance: button;
         overflow: visible;'>Copy</button>
-        <br />
-        <br />
+        <ul></ul>
     </div>
     `;
 
@@ -65,6 +65,7 @@ window.userscript_util.element.NthCopyDiv = (index) => {
         border-color: #4cae4c;
         display: inline-block;
         padding: 6px 12px;
+        margin-bottom: 2em;
         font-size: 14px;
         font-weight: 400;
         line-height: 1.42857143;
@@ -79,8 +80,6 @@ window.userscript_util.element.NthCopyDiv = (index) => {
         border-radius: 4px;
         -webkit-appearance: button;
         overflow: visible;'>Copy</button>
-        <br />
-        <br />
     </div>
     `;
 };
@@ -94,6 +93,7 @@ window.userscript_util.element.ListDiv = `
         line-height: 1.42857143;
         color: #333;
         box-sizing: border-box;'>
+        <ul></ul>
     </div>`;
 
 window.userscript_util.element.NthListDiv = (index) => {
@@ -106,28 +106,47 @@ window.userscript_util.element.NthListDiv = (index) => {
         line-height: 1.42857143;
         color: #333;
         box-sizing: border-box;'>
+        <ul></ul>
     </div>`;
-}
+};
 
-window.userscript_util.element.attachLinkAreatTo = (sel, index) => {
+window.userscript_util.element.attachLinkAreatTo = (sel, index, insertAfterEnd) => {
     const div = document.createElement("div");
-    if( index === "" || index === undefined || index === null) {
+    if (index === "" || index === undefined || index === null) {
         div.innerHTML = userscript_util.element.ClearDiv + userscript_util.element.CopyDiv + userscript_util.element.ListDiv;
-    }else{
+    } else {
         div.innerHTML = userscript_util.element.ClearDiv + userscript_util.element.NthCopyDiv(index) + userscript_util.element.NthListDiv(index);
     }
-    document.querySelector(sel).insertAdjacentElement("beforeend", div);
+    if (insertAfterEnd) {
+        document.querySelector(sel).insertAdjacentElement("afterend", div);
+    } else {
+        document.querySelector(sel).insertAdjacentElement("beforeend", div);
+    }
 };
 
 
-window.userscript_util.element.attachLinkAddress = (href, index) => {
-    document.querySelector(`#article_link_list${index}`).append(href);
-    document.querySelector(`#article_link_list${index}`).append(document.createElement("br"));
+window.userscript_util.element.attachLinkAddress = (linkAnchorEl, index, useLiInsteadOfBr) => {
+    if (useLiInsteadOfBr) {
+        const li = document.createElement("li");
+        li.append(linkAnchorEl);
+        document.querySelector(`#article_link_list${index} ul`).append(li);
+    } else {
+        document.querySelector(`#article_link_list${index}`).append(linkAnchorEl);
+        document.querySelector(`#article_link_list${index}`).append(document.createElement("br"));
+    }
 };
 
 
-window.userscript_util.element.attachLinkAddressExtractedFrom = (fromSel, toSel, lineBreakPredicate, skipPredicate, urlBuilder, indexPredicate, skipBlank) => {
-    if( !indexPredicate ){
+window.userscript_util.element.attachLinkAddressExtractedFrom = (fromSel,
+                                                                 toSel,
+                                                                 lineBreakPredicate,
+                                                                 skipPredicate,
+                                                                 urlBuilder,
+                                                                 indexPredicate,
+                                                                 skipBlank,
+                                                                 useLiInsteadOfBr,
+                                                                 insertAfterEnd) => {
+    if (!indexPredicate) {
         indexPredicate = () => "";
     }
 
@@ -137,28 +156,28 @@ window.userscript_util.element.attachLinkAddressExtractedFrom = (fromSel, toSel,
     document.querySelectorAll(fromSel).forEach((e, i) => {
 
         //여러구역일때, 이전 구역이 끝났으면
-        if( lineBreakPredicate && lineBreakPredicate(i) && i!==0 ){
-            if(!skipBlank){
-                userscript_util.element.attachLinkAddress("about:blank", lastIndex);
+        if (lineBreakPredicate && lineBreakPredicate(i) && i !== 0) {
+            if (!skipBlank) {
+                userscript_util.element.attachLinkAddress("about:blank", lastIndex, useLiInsteadOfBr);
             }
             userscript_util.clipboard.bindClipboardAction(null, lastIndex);
         }
 
-        if( !indexSet.has(indexPredicate(i))){
-            userscript_util.element.attachLinkAreatTo(toSel, indexPredicate(i));
+        if (!indexSet.has(indexPredicate(i))) {
+            userscript_util.element.attachLinkAreatTo(toSel, indexPredicate(i), useLiInsteadOfBr, insertAfterEnd);
             indexSet.add(indexPredicate(i));
         }
 
-        if( skipPredicate && skipPredicate(e, i) ){
+        if (skipPredicate && skipPredicate(e, i)) {
             //do nothing;
-        }else {
+        } else {
             let url;
-            if( urlBuilder ) {
+            if (urlBuilder) {
                 url = urlBuilder(e);
-            }else{
-                url = e.href
+            } else {
+                url = e.href;
             }
-            userscript_util.element.attachLinkAddress(url, indexPredicate(i));
+            userscript_util.element.attachLinkAddress(url, indexPredicate(i), useLiInsteadOfBr);
         }
 
         lastIndex = indexPredicate(i);
@@ -166,8 +185,8 @@ window.userscript_util.element.attachLinkAddressExtractedFrom = (fromSel, toSel,
 
     //통채로 한구역일때 혹은
     //여러 구역일때 마지막 구역은 처리가 안되서 한번 더 처리.
-    if(!skipBlank) {
-        userscript_util.element.attachLinkAddress("about:blank", lastIndex);
+    if (!skipBlank) {
+        userscript_util.element.attachLinkAddress("about:blank", lastIndex, useLiInsteadOfBr);
     }
     userscript_util.clipboard.bindClipboardAction(null, lastIndex);
 
